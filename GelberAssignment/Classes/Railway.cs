cab2e6a0a9014d4abd75c4d7749ea91b3bd39db0
javiercampos <javiercamposcastello@gmail.com>
2024-02-20 18:17:33 -0600

    Error handling in Railway class

--- GelberAssignment/Classes/Railway.cs
+++ GelberAssignment/Classes/Railway.cs
@@ -1,33 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace GelberAssignment.Classes
 {
-	public class Railway
-	{
-		public int NumberOfStations { get; set; }
-		public int TimeToTravel { get; set; }
-		public int FrequencyDepart { get; set; }
-		public int CapacityOfTrains { get; set; }
-		public List<Train> Trains { get; set; }
-		public List<Passenger> PassengersInRailway { get; set; }
+    public class Railway
+    {
+        public int NumberOfStations { get; set; }
+        public int TimeToTravel { get; set; }
+        public int FrequencyDepart { get; set; }
+        public int CapacityOfTrains { get; set; }
+        public List<Train> Trains { get; set; }
+        public List<Passenger> PassengersInRailway { get; set; }
 
-		public Railway(int numberOfStations, int timeToTravel, int frequencyDepart, int capacityOfTrains)
-		{
-			NumberOfStations = numberOfStations;
-			TimeToTravel = timeToTravel;
-			FrequencyDepart = frequencyDepart;
-			CapacityOfTrains = capacityOfTrains;
-			Trains = new List<Train>();
-			PassengersInRailway = new List<Passenger>();
-		}
-
-		public void removeTrains()
-		{
-			this.Trains.RemoveAll(t => (t.Direction == "Right" && t.CurrentPosition == this.NumberOfStations)
-            || (t.Direction == "Left" && t.CurrentPosition == 1));
+        public Railway(int numberOfStations, int timeToTravel, int frequencyDepart, int capacityOfTrains)
+        {
+            NumberOfStations = numberOfStations;
+            TimeToTravel = timeToTravel;
+            FrequencyDepart = frequencyDepart;
+            CapacityOfTrains = capacityOfTrains;
+            Trains = new List<Train>();
+            PassengersInRailway = new List<Passenger>();
         }
 
-
-	}
+        public void removeTrains()
+        {
+            try
+            {
+                this.Trains.RemoveAll(t => (t.Direction == "Right" && t.CurrentPosition == this.NumberOfStations)
+                || (t.Direction == "Left" && t.CurrentPosition == 1));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while removing trains: " + ex.Message);
+            }
+        }
+    }
 }
-
- 
