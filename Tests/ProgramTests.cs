9a830b0b878d95eaa5bef9fe61b693d7458c2bd3
javiercampos <javiercamposcastello@gmail.com>
2024-02-20 18:54:04 -0600

    Added test for ParseFile method

---
+++ Tests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using GelberAssignment.Classes;
+using GelberAssignment;
+namespace Tests;
+[TestClass]
+public class ProgramTests
+{
+	[TestMethod]
+	public void TestParseFile()
+	{
+		// Arrange
+		string content = "";
+
+		// Act
+		string[][] result = Program.ParseFile("");
+
+        // Assert
+        Assert.AreEqual(0, result.Length);
+	}
+}
+
